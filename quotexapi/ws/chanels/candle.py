bcd417ec1ff9a4cf85cd7ca47ea754f7c33fdb23
erikpedra <56580637+erikpedra@users.noreply.github.com>
2023-03-11 12:07:43 +0700

    Create candle.py

---
+++ quotexapi/ws/chanels/candle.py
@@ -0,0 +1,14 @@
+import collections
+from quotexapi.ws.chanels.base import Base
+from quotexapi.constants import constants as OP_code
+from quotexapi.global_value import global_value
+import json
+
+class loadHistoryPeriod(Base):
+    pass
+  
+class changeSymbol(Base):
+    pass
+  
+class unsubfor(Base):
+    pass
