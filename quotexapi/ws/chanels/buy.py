c5a763054341815539e6ac3b487fa82788df5642
erikpedra <56580637+erikpedra@users.noreply.github.com>
2023-03-11 12:09:38 +0700

    Create buy.py

---
+++ quotexapi/ws/chanels/buy.py
@@ -0,0 +1,11 @@
+
+from quotexapi.ws.chanels.base import Base
+from quotexapi.constants import constants as OP_code
+from quotexapi.global_value import global_value
+import json
+
+class buy_binary(Base):
+    pass
+
+class cancelOrder(Base):
+    pass
