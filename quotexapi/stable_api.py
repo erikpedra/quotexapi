edcfedf8fd6ba8df15276755d4b98a26714ccc07
erikpedra <56580637+erikpedra@users.noreply.github.com>
2023-03-11 12:44:16 +0700

    Update stable_api.py

--- quotexapi/stable_api.py
+++ quotexapi/stable_api.py
@@ -7,6 +7,8 @@ import logging
 import operator
 from collections import defaultdict
 from collections import deque
+import json
+import requests
 
 def nested_dict(n, type):
     if n == 1:
@@ -16,21 +18,34 @@ def nested_dict(n, type):
 
 class Quotex:
     __version__ = '2.1'
-    def __init__(self, set_ssid):
+    def __init__(self, email=None, password=None, set_ssid=None, host=None, user_agent=None, cf_bm=None, proxies=None, auto_logout=True):
         self.size = [1, 5, 10, 15, 30, 60, 120, 300, 600, 900, 1800,
                     3600, 7200, 14400, 28800, 43200, 86400, 604800, 2592000]
         self.set_ssid = set_ssid
-        self.suspend = 0.5
-        self.subscribe_candle = []
-        self.subscribe_candle_all_size = []
-        self.subscribe_mood = []
+        self.proxies = proxies
+        self.auto_logout = auto_logout
+        self.email = email
+        self.password = password
+        self.account_mode_isDemo = 1
+        self.client_callback = None
         
-        # --start
-        # self.connect()
-        # this auto function delay too long
+    
         
+    def logout(self):
+        pass
+    
+    def ping_server_go(self):
+        pass
+    
+    def ping_to_server_go_2(self):
+        pass
+    
+    def set_call_back_for_client(self, function):
+        pass
+    
+    def get_ssid(self):
+        pass
         
-
     def change_account(self, Balance_MODE):
         """Change active account `real` or `practice`"""
         real_id = None
@@ -53,15 +68,19 @@ class Quotex:
     # _____________________BUY________________________________
 
     # __________________FOR OPTION____________________________
-    def buy(self, ACTIVES, price, ACTION, expirations):
+    def buy(self, asset, amount, dir, duration):
         """ Buy Binary option""""
         pass
       
     def sell_option(self, options_ids):
         pass
-      
-    def check_win(self, id_number):
+    
+    def check_win_raw(self, ticket, c_function):
+        pass
+        
+    def check_win(self, ticket, polling = (1,)):
         """Check win based id""""
+        time.sleep(polling)
         pass
       
     def get_signal_data(self):
@@ -71,14 +90,17 @@ class Quotex:
     def get_payment(self)
         """ payment Quotex server""""
         pass
-
-      
+    def check_user_data(self):
+        laravel_session = self.api.response_cookies['laravel_session']
+        cookies_dict = {
+            'laravel_session': laravel_session }
+        pass
       
       
     # ________________________________________________________________________
     # _______________________        CANDLE      _____________________________
     # ________________________self.api.getcandles() wss________________________
-    def get_candles(self, ACTIVES, interval, offset, period):
+    def get_candles(self, asset, time, offset, period):
         while True:
             try:
                 pass
@@ -86,25 +108,18 @@ class Quotex:
                 logging.error('**error** get_candles need reconnect')
                 self.connect() #go connect
                 
-    def get_candle_v2(self, ACTIVES, offset):
+    def get_candle_v2(self, asset, period)):
         while True:
             try:
                 pass
             except:
                 logging.error('**error** get_candle_v2 need reconnect')
                 self.connect() #go connect
-    # ------------------------Subscribe ONE SIZE-----------------------
-    def start_candles_one_stream(self, ACTIVE, size):
+                
+                
+    def start_candles_stream(self, asset, size):
         pass
-    def stop_candles_one_stream(self, ACTIVE, size):
-        pass
-    
-      
-    # ------------------------Subscribe ALL SIZE-----------------------
-
-    def start_candles_all_size_stream(self, ACTIVE):
-        pass
-    def stop_candles_all_size_stream(self, ACTIVE):
+    def stop_candles_stream(self, asset):
         pass
       
       
@@ -114,47 +129,10 @@ class Quotex:
     # _____________________REAL TIME CANDLE_________________
     # ______________________________________________________
     #######################################################
-    def start_candles_stream(self, ACTIVE, size, maxdict):
-
-        if size == "all":
-            for s in self.size:
-                pass
-        elif size in self.size:
-            pass
-        else:
-            logging.error('**error** start_candles_stream please input right size')
-            
-     def stop_candles_stream(self, ACTIVE, size):
-        if size == "all":
-            pass
-        elif size in self.size:
-            pass
-        else:
-            logging.error('**error** start_candles_stream please input right size')
-            
-    def get_realtime_candles(self, ACTIVE, size):
-        if size == "all":
-            try:
-                pass
-            except:
-                logging.error('**error** get_realtime_candles() size="all" can not get candle')
-                return False
-        elif size in self.size:
-            try:
-                pass
-            except:
-                logging.error('**error** get_realtime_candles() size=' + str(size) + ' can not get candle')
-                return False
-        else:
-            logging.error('**error** get_realtime_candles() please input right "size"')
-            
-            
-    def re_subscribe_stream(self):
-        try:
-            pass
-        except:
-            pass
-        
+    def get_realtime_candles(self, asset):
+        if asset in self.api.realtime_price and len(self.api.realtime_price[asset]) > 0:
+            return self.api.realtime_price[asset]
+           
       
       
     def connect(self):
@@ -167,7 +145,7 @@ class Quotex:
         check = None
         check, reason = self.api.connect()
         if check == True:
-            self.re_subscribe_stream()
+            
             return True, None
         else:
             return False, reason
