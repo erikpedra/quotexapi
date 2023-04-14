ca7d3d863859bca5ca431d8ce9ad5120ca05d49d
erikpedra <56580637+erikpedra@users.noreply.github.com>
2023-04-14 12:40:32 +0700

    Update client.py

--- quotexapi/ws/client.py
+++ quotexapi/ws/client.py
@@ -14,44 +14,64 @@ class WebsocketClient(object):
             <quotexapi.api.QuotexAPI>`.
         """
         self.api = api
+        self.status = ''
+        self.message_clos = '451-["s_orders/close",{"_placeholder":true,"num":0}]'
+        self.message_cancel = '451-["s_orders/cancel",{"_placeholder":true,"num":0}]'
+        self.cookie_token = 'referer=httpsd%3A%2F%2Fwww.google.com%2F;__cf_bm=' + self.api.cf_bm
         self.wss = websocket.WebSocketApp(
             self.api.wss_url, on_message=self.on_message,
             on_error=self.on_error, on_close=self.on_close,
-            on_open=self.on_open)
+            on_open=self.on_open,header=self.api.header,cookie=self.cookie_token)
 
-    def dict_queue_add(self,dict,maxdict,key1,key2,key3,value):
-        if key3 in dict[key1][key2]:
-                    dict[key1][key2][key3]=value
-        else:
-            while True:
-                try:
-                    dic_size=len(dict[key1][key2])
-                except:
-                    dic_size=0
-                if dic_size<maxdict:
-                    dict[key1][key2][key3]=value
-                    break
-                else:
-                    #del mini key
-                    del dict[key1][key2][sorted(dict[key1][key2].keys(), reverse=False)[0]]   
-
-    def on_message(self, message): # pylint: disable=unused-argument
+    def on_message(self, wss, raw_message): # pylint: disable=unused-argument
         """Method to process websocket messages."""
         global_value.ssl_Mutual_exclusion=True
+        logger = logging.getLogger(__name__)
+        logger.debug(raw_message)
+        if self.api.client_callback != None:
+            self.api.client_callback(raw_message)
+        if raw_message == '40' and global_value.check_auth_finish[id(wss)] == False:
+            logger.debug(global_value.SSID[self.api.object_id])
+            wss.send(global_value.SSID[self.api.object_id])
+            global_value.auth_send_count[self.api.object_id] = global_value.auth_send_count[self.api.object_id] + 1
+        if isinstance(raw_message, str) and '451-' in raw_message:
+            self.api._temp_status = str(raw_message)
         try:
-            logger = logging.getLogger(__name__)
-            message = massage
-            try:
-                message = message[1:]
-                message = message.decode('utf-8')
-                logger.debug(message)
-                message = json.loads(str(message))
+            if self.api._temp_status == '451-["settings/list",{"_placeholder":true,"num":0}]':
+                self.api.settings_list = json.loads(raw_message[4:])
+                self.api._temp_status = ''
+            elif self.api._temp_status == '451-["history/list/v2",{"_placeholder":true,"num":0}]':
+                tt = json.loads(raw_message[1:])
+                self.api.candle_v2_data[tt['asset']] = tt['history']
+                self.api._temp_status = ''
+        finally:
+            pass
+        if raw_message == '42["s_authorization"]':
+            global_value.check_websocket_if_connect[id(wss)] = 1
+            global_value.check_auth_finish[id(wss)] = True
+        try:
+            ok_json = json.loads(raw_message[1:])
+            if 'balance' in ok_json:
+                if ok_json['isDemo'] == 0:
+                    global_value.real_balance[id(wss)] = ok_json['balance']
+                elif ok_json['isDemo'] == 1:
+                    global_value.practice_balance[id(wss)] = ok_json['balance']
+            if 'liveBalance' in ok_json and 'demoBalance' in ok_json:
+                global_value.practice_balance[id(wss)] = ok_json['demoBalance']
+                global_value.real_balance[id(wss)] = ok_json['liveBalance']
+            if 'requestId' in ok_json:
+                self.api.request_data[str(ok_json['requestId'])] = ok_json
+            if self.status == self.message_clos:
                 try:
-                     self.api.buy_id= message["id"]
-                except:
                     pass
-            except:
-                pass
+                finally:
+                    pass
+                self.status = ''
+                if self.status == self.message_cancel:
+                    self.api.check_win_refund_data[ok_json['ticket']] = ok_json
+                    self.status = ''
+                if raw_message == self.message_clos or raw_message == self.message_cancel:
+                    self.status = raw_message
         except:
             pass
         global_value.ssl_Mutual_exclusion=False
@@ -74,7 +94,7 @@ class WebsocketClient(object):
         global_value.check_websocket_if_connect = 1
 
     @staticmethod
-    def on_close(wss):  # pylint: disable=unused-argument
+    def on_close(wss, close_status_code, close_msg):  # pylint: disable=unused-argument
         """Method to process websocket close."""
         logger = logging.getLogger(__name__)
         logger.debug("Websocket connection closed.")
